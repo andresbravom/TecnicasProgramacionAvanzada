cec9962198c255542e04212d5307043281f35f31
Andrés Bravo <andresbravomontes89@gmail.com>
2020-01-09 08:45:45 +0100

    Sin Sincronizacion

---
+++ Ejercicios/Concurrencia/SinSincronizacion/src/ejercicios/Concurrencia/SinSincronizacion/Deposito.java
@@ -0,0 +1,21 @@
+package ejercicios.Concurrencia.SinSincronizacion;
+
+public class Deposito extends Thread {
+	private Cuenta cuenta;
+	private int transacciones;
+	private double importe;
+	
+	public Deposito(Cuenta cuenta, int transacciones, double importe) {
+		this.cuenta = cuenta; 
+		this.transacciones = transacciones;
+		this.importe = importe;	
+	}
+	
+	public void run() {
+		for(int i = 1; i <= this.transacciones; i++) {
+			this.cuenta.deposito(this.importe);  // sin sincronización
+		}
+	}
+}
+
+
