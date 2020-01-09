fcccb80315ea71dfa3e2006406f506ef7e977e8c
Andrés Bravo <andresbravomontes89@gmail.com>
2020-01-09 08:03:47 +0100

    ProtectoraAnimales

--- Ejercicios/ProtectoraAnimales/src/ejercicios/ProtectoraAnimales/Gato.java
+++ Ejercicios/ProtectoraAnimales/src/ejercicios/ProtectoraAnimales/Gato.java
@@ -1,5 +1,25 @@
 package ejercicios.ProtectoraAnimales;
 
-public class Gato {
+public class Gato extends Mascota{
+	
+	private String caracter; 
+	
+	Gato(String name, char genero, String color, String raza, int añoNacimiento, String caracter) {
+		super(name, genero, color, raza, añoNacimiento);
+		this.caracter = caracter;
+	}
 
+	public String getCaracter() {
+		return caracter;
+	}
+
+	public void setCaracter(String caracter) {
+		this.caracter = caracter;
+	}
+	
+	public String getDatos() {
+		return ("Name: " + getName() + "\nGenero: " + getGenero() + "\nColor: " + getColor() 
+				+ "\nRaza: " + getRaza() + "\nAño Nacimiento: " + getAñoNacimiento() + "\nEdad: " 
+				+ getEdad() + "\nCaracter: " + getCaracter());
+	}
 }
--- Ejercicios/ProtectoraAnimales/src/ejercicios/ProtectoraAnimales/Perro.java
+++ Ejercicios/ProtectoraAnimales/src/ejercicios/ProtectoraAnimales/Perro.java
@@ -1,5 +1,27 @@
 package ejercicios.ProtectoraAnimales;
 
-public class Perro {
+public class Perro extends Mascota{
+	
+	private static String tamaño; 
 
+	Perro(String name, char genero, String color, String raza, int añoNacimiento, String tamaño) {
+		super(name, genero, color, raza, añoNacimiento);
+		this.tamaño = tamaño;
+	}
+	public String getTamaño() {
+		return tamaño;
+	}
+
+	public void setTamaño(String tamaño) {
+		this.tamaño = tamaño;
+	}
+	
+	//Se podria hacer de otra forma para llamar a la función de Mascota
+	
+	public String getDatos() {
+		
+		return ("Name: " + getName() + "\nGenero: " + getGenero() + "\nColor: " + getColor() 
+				+ "\nRaza: " + getRaza() + "\nAño Nacimiento: " + getAñoNacimiento() + "\nEdad: " 
+				+ getEdad() + "\nTamaño: " + getTamaño());
+	}
 }
