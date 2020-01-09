ef86157f191140c9c116403383d2c7afea5b1bcb
Andrés Bravo <andresbravomontes89@gmail.com>
2020-01-09 08:54:02 +0100

    Con Sincronizacion

---
+++ Ejercicios/Concurrencia/ConSincronizacion/src/ejercicios/Concurrencia/ConSincronizacion/Deposito.java
@@ -0,0 +1,21 @@
+package ejercicios.Concurrencia.ConSincronizacion;
+
+public class Deposito extends Thread {
+	private Cuenta cuenta;
+	private int transacciones;
+	private double importe;
+	
+	public Deposito(Cuenta cuenta, int transacciones, double importe) {
+		this.cuenta = cuenta; 
+		this.transacciones = transacciones;
+		this.importe = importe;
+		
+	}
+	public void run() {
+		for(int i = 1; i <= this.transacciones; i++) {
+			synchronized(this.cuenta) {
+				this.cuenta.deposito(this.importe);
+				}
+			}
+		}
+	}
